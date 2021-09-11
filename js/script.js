2945577d0d84aa1951b1547ee916ed18224a4edd
NSVenkatesh <85620459+NSVenkatesh@users.noreply.github.com>
2021-09-11 21:22:07 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $(".more_option").click(function(){
+        $(".change").html("less");
+        $(".more").show().animate({"top":0},1000);
+    })
+    $(".cut").click(function(){
+        $(".change").html("more");
+        $(".more").animate({"top":"335px"},1000);
+        // $(".more").hide();
+    })
+    $("#yearly").click(function(){
+        $(".button_overlay").animate({"right":"-10px"},200);
+        $(".button_overlay").animate({"right":0},100);
+        $("#yearly").css("color","white");
+        $("#monthly").css("color","gray");
+        $(".price_down").css("transform","translateY(-55px)")
+        $(".price_up").css("transform","translateY(55px)")
+    })
+    $("#monthly").click(function(){
+        $(".button_overlay").animate({"right":"106px"},200);
+        $(".button_overlay").animate({"right":"96px"},100);
+        $("#yearly").css("color","gray");
+        $("#monthly").css("color","white");
+        $(".price_down").css("transform","translateY(-110px)")
+        $(".price_up").css("transform","translateY(0px)")
+    })
+
+})
